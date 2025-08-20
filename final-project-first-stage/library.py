59f3c8a6a077295afcefe1f742216bb34918a164
Buket Kalfa <buketklf@gmail.com>
2025-08-20 18:31:29 +0300

    Update library.py

--- final-project-first-stage/library.py
+++ final-project-first-stage/library.py
@@ -4,30 +4,34 @@ from book import Book
 class Library:
     def __init__(self, filename="library.json"):
         self.filename = filename
-        self.books = self.load_books()
-
-    def load_books(self):
-        try:
-            with open(self.filename, "r") as f:
-                return [Book(**data) for data in json.load(f)]
-        except FileNotFoundError:
-            return []
-
-    def save_books(self):
-        with open(self.filename, "w") as f:
-            json.dump([book.to_dict() for book in self.books], f, indent=4)
+        self.books = []
+        self.load_books()
 
     def add_book(self, book):
         self.books.append(book)
         self.save_books()
 
     def remove_book(self, isbn):
-        self.books = [book for book in self.books if book.isbn != isbn]
+        self.books = [b for b in self.books if b.isbn != isbn]
         self.save_books()
 
     def list_books(self):
         return self.books
 
     def find_book(self, isbn):
-        return next((book for book in self.books if book.isbn == isbn), None)
+        for book in self.books:
+            if book.isbn == isbn:
+                return book
+        return None
 
+    def load_books(self):
+        try:
+            with open(self.filename, "r") as f:
+                data = json.load(f)
+                self.books = [Book(**item) for item in data]
+        except (FileNotFoundError, json.JSONDecodeError):
+            self.books = []
+
+    def save_books(self):
+        with open(self.filename, "w") as f:
+            json.dump([book.__dict__ for book in self.books], f, indent=4)
