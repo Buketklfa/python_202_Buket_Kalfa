555e624a58f6a67dd0f204b1dcf18f2558a5c8aa
Buket Kalfa <buketklf@gmail.com>
2025-08-20 19:03:10 +0300

    Create book.py

---
+++ final-project-third-stage/book.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "isbn": self.isbn
+        }
