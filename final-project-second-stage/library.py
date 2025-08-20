13343f4684d309be32198d24bbba21fa8b9fc748
Buket Kalfa <buketklf@gmail.com>
2025-08-20 18:44:43 +0300

    Update library.py

--- final-project-second-stage/library.py
+++ final-project-second-stage/library.py
@@ -9,13 +9,14 @@ class Library:
         self.load_books()
 
     def fetch_book_data(self, isbn):
+        url = f"https://openlibrary.org/isbn/{isbn}.json"
         try:
-            response = httpx.get(f"https://openlibrary.org/isbn/{isbn}.json")
+            response = httpx.get(url)
             if response.status_code == 200:
                 data = response.json()
-                title = data.get("title", "Bilinmiyor")
+                title = data.get("title", "Bilinmeyen")
                 authors = data.get("authors", [])
-                author_names = ", ".join([a.get("name", "Bilinmiyor") for a in authors])
+                author_names = ", ".join([a.get("name", "Anonim") for a in authors])
                 return Book(title, author_names, isbn)
             else:
                 return None
@@ -30,4 +31,27 @@ class Library:
             return book
         return None
 
-    # Diğer metodlar aynı
+    def remove_book(self, isbn):
+        self.books = [b for b in self.books if b.isbn != isbn]
+        self.save_books()
+
+    def list_books(self):
+        return self.books
+
+    def find_book(self, isbn):
+        for book in self.books:
+            if book.isbn == isbn:
+                return book
+        return None
+
+    def load_books(self):
+        try:
+            with open(self.filename, "r") as f:
+                data = json.load(f)
+                self.books = [Book(**item) for item in data]
+        except FileNotFoundError:
+            self.books = []
+
+    def save_books(self):
+        with open(self.filename, "w") as f:
+            json.dump([book.__dict__ for book in self.books], f, indent=4)
