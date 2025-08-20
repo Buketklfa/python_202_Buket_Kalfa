e05be6dd317e3438b3fd63873814e0ce59fbbb3d
Buket Kalfa <buketklf@gmail.com>
2025-08-20 18:37:54 +0300

    Update book.py

--- final-project-first-stage/book.py
+++ final-project-first-stage/book.py
@@ -1,8 +1,37 @@
-class Book:
-    def __init__(self, title, author, isbn):
-        self.title = title
-        self.author = author
-        self.isbn = isbn
+import json
+from book import Book
 
-    def __str__(self):
-        return f"{self.title} by {self.author} (ISBN: {self.isbn})"
+class Library:
+    def __init__(self, filename="library.json"):
+        self.filename = filename
+        self.books = []
+        self.load_books()
+
+    def add_book(self, book):
+        self.books.append(book)
+        self.save_books()
+
+    def remove_book(self, isbn):
+        self.books = [b for b in self.books if b.isbn != isbn]
+        self.save_books()
+
+    def list_books(self):
+        return self.books
+
+    def find_book(self, isbn):
+        for book in self.books:
+            if book.isbn == isbn:
+                return book
+        return None
+
+    def load_books(self):
+        try:
+            with open(self.filename, "r") as f:
+                data = json.load(f)
+                self.books = [Book(**item) for item in data]
+        except FileNotFoundError:
+            self.books = []
+
+    def save_books(self):
+        with open(self.filename, "w") as f:
+            json.dump([book.__dict__ for book in self.books], f, indent=4)
