3a89219451df318ea03dd23712f2ccb0b0629aaa
Buket Kalfa <buketklf@gmail.com>
2025-08-20 18:32:16 +0300

    Update tests.py

--- final-project-first-stage/tests.py
+++ final-project-first-stage/tests.py
@@ -3,8 +3,13 @@ from book import Book
 
 def test_add_and_find_book():
     lib = Library("test_library.json")
+    lib.books = []  # Temiz başlangıç
     book = Book("Test", "Tester", "123")
     lib.add_book(book)
     assert lib.find_book("123").title == "Test"
+
+def test_remove_book():
+    lib = Library("test_library.json")
+    lib.books = [Book("Test", "Tester", "123")]
     lib.remove_book("123")
-    
+    assert lib.find_book("123") is None
