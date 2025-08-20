71748a116a82c72b5e83c6a619054f7c4eeb5ab7
Buket Kalfa <buketklf@gmail.com>
2025-08-20 19:05:49 +0300

    Update test_api.py

--- final-project-third-stage/test_api.py
+++ final-project-third-stage/test_api.py
@@ -7,10 +7,6 @@ def test_get_books():
     response = client.get("/books")
     assert response.status_code == 200
 
-def test_post_book():
-    response = client.post("/books", json={"isbn": "9780140328721"})
-    assert response.status_code in [200, 404]
-
-def test_delete_book():
-    response = client.delete("/books/9780140328721")
-    assert response.status_code in [200, 404]
+def test_add_invalid_isbn():
+    response = client.post("/books", params={"isbn": "0000000000"})
+    assert response.status_code == 404
