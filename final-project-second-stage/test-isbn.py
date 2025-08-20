8293a5281e654f612a9aaaa1939b2c553d86da02
Buket Kalfa <buketklf@gmail.com>
2025-08-20 18:45:29 +0300

    Update test-isbn.py

--- final-project-second-stage/test-isbn.py
+++ final-project-second-stage/test-isbn.py
@@ -1,12 +1,12 @@
 from library import Library
 
 def test_valid_isbn():
-    lib = Library("test.json")
-    book = lib.add_book("9780140328721")  # Matilda by Roald Dahl
+    lib = Library("test_library.json")
+    book = lib.fetch_book_data("9780140328721")
     assert book is not None
-    assert "Matilda" in book.title
+    assert book.title != ""
 
 def test_invalid_isbn():
-    lib = Library("test.json")
-    book = lib.add_book("0000000000000")
+    lib = Library("test_library.json")
+    book = lib.fetch_book_data("0000000000000")
     assert book is None
